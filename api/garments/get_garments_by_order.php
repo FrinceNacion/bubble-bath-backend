3375ed898e27ea7dfeacd484320165082b6bf2ed
Frince Louie L. Nacion <louienacion9@gmail.com>
2026-05-11 18:02:54 +0800

    Refactor: implement endpoint to retrieve garments by order ID

--- api/garments/get_garments_by_order.php
+++ api/garments/get_garments_by_order.php
@@ -1,14 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: http://localhost:5173");
-header("Access-Control-Allow-Credentials: true");
-header("Access-Control-Allow-Methods: POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
-header('Content-Type: application/json');
-
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(200);
-    exit();
-}
+require_once __DIR__ . '/../../config/headers.php';
 
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     http_response_code(405);
@@ -16,9 +7,9 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     exit();
 }
 
-require_once 'authenticate.php';
+require_once __DIR__ . '/../../middleware/authenticate.php';
 
-require_once 'connect_db.php';
+require_once __DIR__ . '/../../config/database.php';
 
 $input = json_decode(file_get_contents("php://input"), true);
 
